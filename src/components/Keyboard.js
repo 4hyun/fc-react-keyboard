4b36274972f13a01ed0cbcb04497b67e73c2d9d0
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-07 19:05:15 +0900

    update Keyboard static method names

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,19 @@
 export class Keyboard {
   #switchEl;
+  #fontSelectEl;
+  #keyboardEl;
   constructor() {
-    this.#addSwitchEl();
+    this.#initElements();
     this.#registerEvents();
   }
-  #addSwitchEl() {
+  #initElements() {
     this.#switchEl = document.getElementById("switch");
+    /* TODO: fontSelectEl */
+    /* TODO: containerEl */
   }
   #registerEvents() {
     this.#switchEl.addEventListener("click", () => {});
+    /* fontSelect */
+    /* containerEl */
   }
 }
